6531a8c6e99136c083388bc0ed438176c4520b8e
mrkm <murakami@lafla.co.jp>
2015-02-08 16:40:54 +0900

    remove unused files

--- apps/admin/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, url
-from core.views import TopView, DeleteView
-
-
-urlpatterns = patterns('',
-    url(r'^$', TopView.as_view(), name='home'),
-    url(r'^delete/(?P<pk>\d+)/?$', DeleteView.as_view(), name='delete'),
-)
